1e2d416605ced3c33089dbaadebae79826fb97fb
Ryan Ho <ryan@horyan.com>
2020-04-28 03:19:20 -0400

    Remove sample data and Nyaa.si acknowledgement. Add basic torrent form.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+function addRow() {
+  let name, uri, size, seeders, leechers;
+  const form = document.forms["torrent-form"];
+  const nameList = [name, uri, size, seeders, leechers]; 
+  const valueList = [form["name"].value,
+                    form["uri"].value,
+                    form["size"].value,
+                    form["seeders"].value,
+                    form["leechers"].value];
+
+  const tbody = document.getElementById("torrent-tbody");
+  const newRow = document.createElement("tr");
+
+  for (let i = 0; i < nameList.length; i++) {
+    nameList[i] = document.createElement("td");
+    nameList[i].innerHTML = valueList[i]; 
+    newRow.appendChild(nameList[i]);
+  }
+  tbody.appendChild(newRow);
+}
+
 function toggleTheme() {
   const theme = document.getElementById("btn-switch");
   if (document.body.classList.toggle("dark") === true) {
