8f3707bf7513f7b019218a47ad95046f6fc8f6a7
Ryan Ho <ryan@horyan.com>
2020-04-29 22:21:31 -0400

    Add caret buttons without sort functionality

--- js/main.js
+++ js/main.js
@@ -20,18 +20,83 @@ function addRow() {
   inputRow.parentNode.insertBefore(newRow, inputRow);
 
   // clear input fields each time a new row is added
+  // TODO: change invalidation styling
   const inputNames = ["name", "uri", "size", "seeders", "leechers"];
   for (let i = 0; i < inputNames.length; i++) {
     torrentForm[inputNames[i]].value = "";
   }
 }
 
+function inputReset() {
+  // clear non-empty input fields
+  // TODO: change invalidation styling
+  const inputNames = ["name", "uri", "size", "seeders", "leechers"];
+  torrentForm = document.forms["torrent-form"];
+  for (let i = 0; i < inputNames.length; i++) {
+    if (torrentForm[inputNames[i]].value != "")
+      torrentForm[inputNames[i]].value = "";
+  }
+}
+
 function toggleTheme() {
-  const theme = document.getElementById("btn-switch");
+  const theme = document.getElementById("btn-theme");
   if (document.body.classList.toggle("dark") === true) {
     theme.innerHTML = "Light Mode";
   }
   else {
     theme.innerHTML = "Dark Mode";
+  }
+}
+
+// TODO: avoid repitition; combine into a single function
+// TODO: add asc/desc sort functionality
+function toggleNameSort() {
+  const sortButton = document.getElementById("btn-name");
+  if (sortButton.innerHTML === "▷") {
+    sortButton.innerHTML = "&#9651";
+  } else if (sortButton.innerHTML === "△") {
+    sortButton.innerHTML = "&#9661";
+  } else {
+    sortButton.innerHTML = "&#9655";
+  }
+}
+function toggleUriSort() {
+  const sortButton = document.getElementById("btn-uri");
+  if (sortButton.innerHTML === "▷") {
+    sortButton.innerHTML = "&#9651";
+  } else if (sortButton.innerHTML === "△") {
+    sortButton.innerHTML = "&#9661";
+  } else {
+    sortButton.innerHTML = "&#9655";
+  }
+}
+function toggleSizeSort() {
+  const sortButton = document.getElementById("btn-size");
+  if (sortButton.innerHTML === "▷") {
+    sortButton.innerHTML = "&#9651";
+  } else if (sortButton.innerHTML === "△") {
+    sortButton.innerHTML = "&#9661";
+  } else {
+    sortButton.innerHTML = "&#9655";
+  }
+}
+function toggleSeedersSort() {
+  const sortButton = document.getElementById("btn-seeders");
+  if (sortButton.innerHTML === "▷") {
+    sortButton.innerHTML = "&#9651";
+  } else if (sortButton.innerHTML === "△") {
+    sortButton.innerHTML = "&#9661";
+  } else {
+    sortButton.innerHTML = "&#9655";
+  }
+}
+function toggleLeechersSort() {
+  const sortButton = document.getElementById("btn-leechers");
+  if (sortButton.innerHTML === "▷") {
+    sortButton.innerHTML = "&#9651";
+  } else if (sortButton.innerHTML === "△") {
+    sortButton.innerHTML = "&#9661";
+  } else {
+    sortButton.innerHTML = "&#9655";
   }
 }
