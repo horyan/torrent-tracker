eaf9d333486368bd036207de3f69a9f3d4b18514
Ryan Ho <ryan@horyan.com>
2020-05-03 18:28:27 -0400

    Modify various comments

--- js/main.js
+++ js/main.js
@@ -1,12 +1,3 @@
-/* Global */
-
-// onclick: toggle column sort |TODO: add asc/desc sort logic
-const sortButtons = ['btn-name', 'btn-uri', 'btn-size', 'btn-seeders', 'btn-leechers'];
-for (let i = 0; i < sortButtons.length; i++) {
-  document.getElementById(sortButtons[i]).addEventListener('click', toggleSort);
-}
-
-
 /* Declarations */
 
 function addNewRow() {
@@ -17,7 +8,7 @@ function addNewRow() {
   const newTdContents = [torrentForm['name'].value, torrentForm['uri'].value, torrentForm['size'].value,
                         torrentForm['seeders'].value, torrentForm['leechers'].value,
                         '<button type="button">Edit</button>' +
-                        '<button type="button" onclick="deleteRow(this)">Delete</button>']; // fix last item clutter
+                        '<button type="button" onclick="deleteRow(this)">Delete</button>']; // TODO: fix HTML clutter
 
   const newTr = document.createElement('tr');
   newTr.className = 'user-row';
@@ -28,13 +19,12 @@ function addNewRow() {
     newTr.appendChild(newTds[i]);
   }
   
-  newTds[newTds.length-1].addEventListener('mouseenter', hoverEditOn);
-  //newTds[newTds.length-1].setAttribute('onmouseenter', 'hoverEditOn(this)'); // assign attr to last td
+  newTds[newTds.length-1].addEventListener('mouseenter', hoverEditOn); // listen for mouseenter on last td
   
   const torrentRow = document.getElementById('torrent-row');
   torrentRow.parentNode.insertBefore(newTr, torrentRow); // insert new row before torrentRow
   
-  //TODO: remove invalidation styles in this scenario
+  // fix invalidation styling in this scenario
   const torrentNames = ['name', 'uri', 'size', 'seeders', 'leechers'];
   for (let i = 0; i < torrentNames.length; i++) {
     torrentForm[torrentNames[i]].value = ''; // clear input text fields
@@ -43,7 +33,7 @@ function addNewRow() {
 
 
 function deleteRow(e) {
-  const userRow = e.parentElement.parentElement; // fix using unique button id attr
+  const userRow = e.parentElement.parentElement; // TODO: specify unique button id attr
   userRow.remove(userRow);
 }
 
@@ -85,12 +75,18 @@ document.getElementById('btn-theme').onclick = () => {
 };
 
 
-// onsubmit: add-button fires addNewRow() and doesn't send form anywhere
+// onclick: toggle column sort |TODO: add asc/desc sort logic
+const sortButtons = ['btn-name', 'btn-uri', 'btn-size', 'btn-seeders', 'btn-leechers'];
+for (let i = 0; i < sortButtons.length; i++) {
+  document.getElementById(sortButtons[i]).addEventListener('click', toggleSort);
+}
+
+
+// onsubmit: add-button fires addNewRow() and don't send form anywhere
 document.getElementById('torrent-form').onsubmit = () => { addNewRow(); return false; };
 
 
-// onclick: remove non-empty input fields
-// TODO: remove invalidation style in this scenario
+// onclick: remove non-empty input fields |TODO: fix invalidation styling in this scenario
 document.getElementById('btn-clear').onclick = () => {
   const torrentNames = ['name', 'uri', 'size', 'seeders', 'leechers'];
   const torrentForm = document.forms['torrent-form'];
@@ -101,7 +97,7 @@ document.getElementById('btn-clear').onclick = () => {
 };
 
 
-// onclick: add 5 new test rows to table (TESTING PURPOSES)
+// onclick: add 5 new test rows to table (for testing)
 document.getElementById('add5').onclick = () => {
   const torrentForm = document.forms['torrent-form'];
   const numNewRows = 5;
