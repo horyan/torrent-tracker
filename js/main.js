bc51da91900571897920f6b986f4643545dbc59d
Ryan Ho <ryan@horyan.com>
2020-05-03 18:01:38 -0400

    Use addEventListener where possible

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+/* Global */
+
+// onclick: toggle column sort |TODO: add asc/desc sort logic
+const sortButtons = ['btn-name', 'btn-uri', 'btn-size', 'btn-seeders', 'btn-leechers'];
+for (let i = 0; i < sortButtons.length; i++) {
+  document.getElementById(sortButtons[i]).addEventListener('click', toggleSort);
+}
+
+
 /* Declarations */
 
 function addNewRow() {
@@ -19,7 +28,8 @@ function addNewRow() {
     newTr.appendChild(newTds[i]);
   }
   
-  newTds[newTds.length-1].setAttribute('onmouseenter', 'hoverEditOn(this)'); // assign attr to last td
+  newTds[newTds.length-1].addEventListener('mouseenter', hoverEditOn);
+  //newTds[newTds.length-1].setAttribute('onmouseenter', 'hoverEditOn(this)'); // assign attr to last td
   
   const torrentRow = document.getElementById('torrent-row');
   torrentRow.parentNode.insertBefore(newTr, torrentRow); // insert new row before torrentRow
@@ -33,37 +43,37 @@ function addNewRow() {
 
 
 function deleteRow(e) {
-  const userRow = e.parentElement.parentElement; // TODO: specify unique button id attr
+  const userRow = e.parentElement.parentElement; // fix using unique button id attr
   userRow.remove(userRow);
 }
 
 
 function toggleSort(e) {
-  if (e.innerHTML === '\u25b7' /*'▷'*/) {
-    e.innerHTML = '&#9651';
+  if (e.target.innerHTML === '\u25b7' /*'▷'*/) {
+    e.target.innerHTML = '&#9651';
   } else if (e.innerHTML === '\u25b3' /*'△'*/) {
-    e.innerHTML = '&#9661';
+    e.target.innerHTML = '&#9661';
   } else {
-    e.innerHTML = '&#9655';
+    e.target.innerHTML = '&#9655';
   }
 }
 
 
 function hoverEditOn(e) {
-  e.firstElementChild.textContent = 'Save'; // remap edit to save
+  e.target.firstElementChild.textContent = 'Save'; // remap edit to save
 
   const cancel = document.createElement('button');
   cancel.setAttribute('type', 'button')
   cancel.innerHTML = 'Cancel';
 
-  e.insertBefore(cancel, e.firstElementChild); // add cancel button before save button
-  e.onmouseleave = function() {hoverEditOff(this);}; // similar issue with arrow conversion
+  e.target.insertBefore(cancel, e.target.firstElementChild); // add cancel button before save button
+  e.target.addEventListener('mouseleave', hoverEditOff);
 }
 
 
 function hoverEditOff(e) {
-  e.removeChild(e.firstElementChild); // remove exposed cancel button
-  e.firstElementChild.textContent = 'Edit'; // remap save to edit
+  e.target.removeChild(e.target.firstElementChild); // remove exposed cancel button
+  e.target.firstElementChild.textContent = 'Edit'; // remap save to edit
 }
 
 
@@ -89,15 +99,6 @@ document.getElementById('btn-clear').onclick = () => {
     if (torrentForm[torrentNames[i]].value !== '') torrentForm[torrentNames[i]].value = '';
   }
 };
-
-
-// TODO: add asc/desc sort logic
-// (e) => {toggleSort(this);}; syntax issue even if function parameter and 'this' argument are excluded
-document.getElementById('btn-name').onclick = function() {toggleSort(this);}; // won't work with implicit this
-document.getElementById('btn-uri').onclick = function() {toggleSort(this);};
-document.getElementById('btn-size').onclick = function() {toggleSort(this);};
-document.getElementById('btn-seeders').onclick = function() {toggleSort(this);};
-document.getElementById('btn-leechers').onclick = function() {toggleSort(this);};
 
 
 // onclick: add 5 new test rows to table (TESTING PURPOSES)
