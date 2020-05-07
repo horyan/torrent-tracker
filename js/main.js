d5f2b3a15f66fa949499afa5185a043e2f9cd0fc
Ryan Ho <ryan@horyan.com>
2020-05-07 00:28:37 -0400

    Re-implement core functions and id generation
    - Generate unique row ids
    - Update test button to add rows from array of objects
    - Begin revision of edit button functionality

--- js/main.js
+++ js/main.js
@@ -2,14 +2,16 @@
 
 function populateTable(torrents) {
   const tableBody = document.getElementById('torrent-tbody');
-  for (let i = 0; i < torrents.length; ++i){
-    tableBody.appendChild(constructRowTemplate(torrents[i], i)); // insert row child
+  let i = document.getElementsByTagName('tr').length-1; // index for existing rows minus input row
+  let j = 0;
+  for (i, j; j < torrents.length; ++i, ++j){
+    tableBody.appendChild(constructRowTemplate(torrents[j], i)); // insert row child
   }
 }
 
 function constructRowTemplate(torrent, index){
   const tr = document.createElement('tr');
-  tr.setAttribute('id', `row-${index}`); // TODO: fix index collision with next batch of rows
+  tr.setAttribute('id', `row-${index}`);
 
   // convert into array of object's values (iterable, has length)
   const torrentValues = Object.values(torrent);
@@ -24,6 +26,7 @@ function constructRowTemplate(torrent, index){
   const deleteBtn = document.createElement('button');
   
   editBtn.textContent = 'Edit';
+  editBtn.setAttribute('type', 'button'); // how come delete button isn't firing the same submit issue?
   editBtn.addEventListener('click', enableRowEdit);
   deleteBtn.textContent = 'Delete';
   deleteBtn.addEventListener('click', deleteRow);
@@ -36,7 +39,7 @@ function constructRowTemplate(torrent, index){
 }
 
 function deleteRow(e) {
-  const userRow = e.target.parentElement.parentElement;
+  const userRow = e.target.parentElement.parentElement; // versus row-id?
   userRow.remove(userRow);
 }
 
@@ -50,16 +53,18 @@ function toggleSort(e) {
   }
 }
 
-// TODO: implement click listener: transform row tds into inputs, need cancel/save/delete buttons, 
+// TODO: transform row tds into inputs, need functional cancel/save buttons
 function enableRowEdit(e) {
-  e.target.firstElementChild.textContent = 'Save'; // remap edit to save
+  e.target.textContent = 'Save'; // remap edit to save
+  e.target.removeEventListener('click', enableRowEdit);
+  // TODO: e.target.addEventListener('click', saveRow)
 
   const cancel = document.createElement('button');
   cancel.setAttribute('type', 'button');
-  cancel.innerHTML = 'Cancel';
+  cancel.textContent = 'Cancel';
 
-  e.target.insertBefore(cancel, e.target.firstElementChild); // add cancel button before save button
-  e.target.addEventListener('mouseleave', disableRowEdit);
+  e.target.parentNode.insertBefore(cancel, e.target); // add cancel button before save button
+  //e.target.addEventListener('click', disableRowEdit);
 }
 
 function disableRowEdit(e) {
@@ -84,50 +89,43 @@ for (let i = 0; i < sortButtons.length; ++i) {
   document.getElementById(sortButtons[i]).addEventListener('click', toggleSort);
 }
 
-// onclick: remove non-empty input fields |TODO: type="reset", fix invalidation styling in this scenario
-document.getElementById('btn-clear').addEventListener('click', () => {
-  const torrentNames = ['name', 'uri', 'size', 'seeders', 'leechers'];
-  const torrentForm = document.forms['torrent-form'];
-
-  for (let i = 0; i < torrentNames.length; ++i) {
-    if (torrentForm[torrentNames[i]].value !== '') torrentForm[torrentNames[i]].value = '';
-  }
-});
-
 document.getElementById('json-test').addEventListener('click', () => {
-  const testTorrents = [
-    {
-      name: "test",
-      uri: "test.test.com",
-      size: 12345,
-      seeders: 123,
-      leechers: 123
-    },
-    {
-      name: "test 1234",
-      uri: "test.ioi.com",
-      size: 1232,
-      seeders: 23,
-      leechers: 23
-    }
-  ];
-  populateTable(testTorrents);
+  const dynamicTorrents = [];
+  let i = document.getElementsByTagName('tr').length-1; // existing rows minutes input row
+  let j = 0;
+  for (i, j; j < 10; ++i, ++j){
+    let temp = {
+                name: `torrent-${i}`,
+                uri: `example-${i+1}.torrents.com`,
+                size: `${i+2} GiB`,
+                seeders: `${i+3}`,
+                leechers: `${i+4}`
+                }; 
+    dynamicTorrents.push(temp);
+  }
+
+  populateTable(dynamicTorrents);
 });
 
 // onsubmit: add row from inputs
 document.getElementById('torrent-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const torrentJson = [{name:'', uri:'', size:'', seeders:'', leechers:''}];
   const torrentInputs = document.querySelectorAll('#torrent-tr input');
+  const torrent = [{name:'', uri:'', size:'', seeders:'', leechers:''}];
 
-  torrentJson[0].name = torrentInputs[0].value;
-  torrentJson[0].uri = torrentInputs[1].value;
-  torrentJson[0].size = torrentInputs[2].value;
-  torrentJson[0].seeders = torrentInputs[3].value;
-  torrentJson[0].leechers = torrentInputs[4].value;
+  torrent[0].name = torrentInputs[0].value;
+  torrent[0].uri = torrentInputs[1].value;
+  torrent[0].size = torrentInputs[2].value;
+  torrent[0].seeders = torrentInputs[3].value;
+  torrent[0].leechers = torrentInputs[4].value;
 
-  populateTable(torrentJson);
+  populateTable(torrent);
+
+  // clear inputs |TODO: hide invalidation style for this scenario
+  for (let i = 0; i < torrentInputs.length; ++i){
+    torrentInputs[i].value = '';
+  }
 });
 
 // TODO: consider using named functions with addEventListener
