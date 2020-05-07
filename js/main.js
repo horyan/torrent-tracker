25099d415947bf81e5587abfe5667e74eec5df3c
Ryan Ho <ryan@horyan.com>
2020-05-07 01:22:58 -0400

    Rename elements, functions. Plan edit logic.
    - Remove and rename various ids
    - Rename edit-related function names
    - Plan logic for edit/cancel/save states

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 /* Declarations */
 
+
 function populateTable(torrents) {
-  const tableBody = document.getElementById('torrent-tbody');
-  let i = document.getElementsByTagName('tr').length-1; // index for existing rows minus input row
-  let j = 0;
-  for (i, j; j < torrents.length; ++i, ++j){
-    tableBody.appendChild(constructRowTemplate(torrents[j], i)); // insert row child
+  const tableBody = document.getElementById('torrent-data');
+  let i = 0, j = document.getElementsByTagName('tr').length-1; // index for existing rows minus input row
+  for (i, j; i < torrents.length; ++i, ++j){
+    tableBody.appendChild(constructRowTemplate(torrents[i], j)); // insert row child
   }
 }
+
 
 function constructRowTemplate(torrent, index){
   const tr = document.createElement('tr');
@@ -26,8 +27,8 @@ function constructRowTemplate(torrent, index){
   const deleteBtn = document.createElement('button');
   
   editBtn.textContent = 'Edit';
-  editBtn.setAttribute('type', 'button'); // how come delete button isn't firing the same submit issue?
-  editBtn.addEventListener('click', enableRowEdit);
+  editBtn.setAttribute('type', 'button'); // delete btn isn't firing the same submit issue?
+  editBtn.addEventListener('click', enableEdit);
   deleteBtn.textContent = 'Delete';
   deleteBtn.addEventListener('click', deleteRow);
   
@@ -38,10 +39,12 @@ function constructRowTemplate(torrent, index){
   return tr;
 }
 
+
 function deleteRow(e) {
   const userRow = e.target.parentElement.parentElement; // versus row-id?
   userRow.remove(userRow);
 }
+
 
 function toggleSort(e) {
   if (e.target.innerHTML === '\u00AB') {
@@ -53,29 +56,48 @@ function toggleSort(e) {
   }
 }
 
-// TODO: transform row tds into inputs, need functional cancel/save buttons
-function enableRowEdit(e) {
+
+function enableEdit(e) {
+  // TODO: transform row tds into inputs
+  //
+
   e.target.textContent = 'Save'; // remap edit to save
-  e.target.removeEventListener('click', enableRowEdit);
-  // TODO: e.target.addEventListener('click', saveRow)
+  e.target.removeEventListener('click', enableEdit);
+  //e.target.addEventListener('click', saveEdit)
 
   const cancel = document.createElement('button');
   cancel.setAttribute('type', 'button');
   cancel.textContent = 'Cancel';
 
   e.target.parentNode.insertBefore(cancel, e.target); // add cancel button before save button
-  //e.target.addEventListener('click', disableRowEdit);
+  //e.target.addEventListener('click', disableEdit);
 }
+
+
+function cancelEdit(){
+  // TODO: remove cancel button
+  // remap save textContent/event handler callback: Edit/disableEdit
+}
+
+
+function saveEdit(){
+// TODO: store input values into td.textContent
+// remove input element
+// cancelEdit()
+}
+
 
 function disableRowEdit(e) {
   e.target.removeChild(e.target.firstElementChild); // remove exposed cancel button
   e.target.firstElementChild.textContent = 'Edit'; // remap save to edit
 }
 
+
 /* Operators */
 
+
 // onclick: toggle color theme
-document.getElementById('btn-theme').addEventListener('click', () => {
+document.getElementById('theme-btn').addEventListener('click', () => {
   if (document.body.classList.toggle('dark') === true) {
     this.textContent = 'Light Mode';
   } else {
@@ -83,35 +105,38 @@ document.getElementById('btn-theme').addEventListener('click', () => {
   }
 });
 
+
 // onclick: toggle column sort |TODO: add asc/desc sort logic
-const sortButtons = ['btn-name', 'btn-uri', 'btn-size', 'btn-seeders', 'btn-leechers'];
+const sortButtons = ['name-sort', 'uri-sort', 'size-sort', 'seeders-sort', 'leechers-sort'];
 for (let i = 0; i < sortButtons.length; ++i) {
   document.getElementById(sortButtons[i]).addEventListener('click', toggleSort);
 }
 
+
 document.getElementById('json-test').addEventListener('click', () => {
   const dynamicTorrents = [];
-  let i = document.getElementsByTagName('tr').length-1; // existing rows minutes input row
-  let j = 0;
-  for (i, j; j < 10; ++i, ++j){
-    let temp = {
-                name: `torrent-${i}`,
-                uri: `example-${i+1}.torrents.com`,
-                size: `${i+2} GiB`,
-                seeders: `${i+3}`,
-                leechers: `${i+4}`
-                }; 
+
+  let i =  0, j = document.getElementsByTagName('tr').length-1; // existing rows minutes input row
+  for (i, j; i < 10; ++i, ++j){
+    const temp = {
+                  name: `torrent-${j}`,
+                  uri: `example-${j}.torrents.com`,
+                  size: `${j} GiB`,
+                  seeders: `${j}`,
+                  leechers: `${j}`
+                  }; 
     dynamicTorrents.push(temp);
   }
 
   populateTable(dynamicTorrents);
 });
 
+
 // onsubmit: add row from inputs
 document.getElementById('torrent-form').addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const torrentInputs = document.querySelectorAll('#torrent-tr input');
+  const torrentInputs = document.querySelectorAll('#torrent-input input');
   const torrent = [{name:'', uri:'', size:'', seeders:'', leechers:''}];
 
   torrent[0].name = torrentInputs[0].value;
@@ -127,6 +152,3 @@ document.getElementById('torrent-form').addEventListener('submit', (e) => {
     torrentInputs[i].value = '';
   }
 });
-
-// TODO: consider using named functions with addEventListener
-// TODO: clipboard copy name/uri data cell on dblclick event
